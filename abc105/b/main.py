91f6d415528d9e6a4e0b27374f4650bebd484309
ikepon <tatsuyanoheya@gmail.com>
2022-06-20 22:16:13 +0900

    feat: 105b

--- abc105/b/main.py
+++ abc105/b/main.py
@@ -1,5 +1,6 @@
-a = input()
-a1, a2, a3 = list(map(int, input().split()))
+a = int(input())
 
-
-
+if a == 1 or a == 2 or a == 3 or a == 5 or a == 6 or a == 9 or a == 10 or a == 13 or a == 17:
+    print('No')
+else:
+    print('Yes')
