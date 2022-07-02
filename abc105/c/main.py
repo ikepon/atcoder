a9ffdcccca9fd4189841d437f8991d613af8246d
ikepon <tatsuyanoheya@gmail.com>
2022-07-02 23:02:43 +0900

    106

--- abc105/c/main.py
+++ abc105/c/main.py
@@ -1,5 +1,18 @@
-a = input()
-a1, a2, a3 = list(map(int, input().split()))
+n = int(input())
 
+ans = []
 
+if n % 2 == 1:
+    ans.append(1)
+else:
+    ans.append(0)
 
+for i in range(1, 35):
+    a, mod = divmod(n, -2 ** i)
+    print(a, mod)
+    ans.insert(0, a)
+    n = mod
+
+ans_p = ''.join(map(str, ans))
+
+print(int(ans_p))
